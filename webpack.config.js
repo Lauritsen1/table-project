e61c6baac9cc0c0d544465fe2f08457bfcc9bd6d
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2021-04-03 17:31:41 +0200

    Test

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const { watch } = require('fs');
+const path = require('path');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ],
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 9000
+    },
+
+    watch: true
+}
